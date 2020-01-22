bea808d8abbd68da6c93717de2e37e32cdf37261
BuildTools <unconfigured@null.spigotmc.org>
2020-01-21 18:48:16 -0800

    1/21 Commit 2

---
+++ project-1/js/script.js
@@ -0,0 +1,16 @@
+// $(window).scroll(function() {
+
+// 	if ($(this).scrollTop() > 500 && $(this).scrollTop() < 550){
+// 		$("#page1").css('visibility', 'hidden');
+// 		$("#page2").css('visibility', 'visible');
+// 	}
+// 	else if ($(this).scrollTop() > 550 ){
+// 			$("#page1").css('visibility', 'visible');
+// 			$("#page2").css('visibility', 'hidden');
+// 	} 
+// 	else {
+// 		$("#page1").css('visibility', 'visible');
+// 		$("#page2").css('visibility', 'hidden');
+// 	}
+
+// });
