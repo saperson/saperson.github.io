10756da07e81932e73292d2c696866c3fb9a49fc
BuildTools <unconfigured@null.spigotmc.org>
2020-01-27 15:49:02 -0800

    1/27 Commit 4

--- project-1/js/script.js
+++ project-1/js/script.js
@@ -1,20 +1,11 @@
-// $(window).scroll(function() {
-
-// 	if ($(this).scrollTop() > 500 && $(this).scrollTop() < 550){
-// 		$("#page1").css('visibility', 'hidden');
-// 		$("#page2").css('visibility', 'visible');
-// 	}
-// 	else if ($(this).scrollTop() > 550 ){
-// 			$("#page1").css('visibility', 'visible');
-// 			$("#page2").css('visibility', 'hidden');
-// 	} 
-// 	else {
-// 		$("#page1").css('visibility', 'visible');
-// 		$("#page2").css('visibility', 'hidden');
-// 	}
-
-// });
-
 $( function() {
 	$( "#draggable" ).draggable({ axis: "y" });
 } );
+
+$( function() {
+	$('#draggable').on('drag', function() {
+		$(this).addClass('dragcomplete');
+	});
+});
+
+// if top=-330px
