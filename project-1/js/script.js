89cf7a1d4162742078df1778ce8034c89b4f5f25
BuildTools <unconfigured@null.spigotmc.org>
2020-01-27 14:42:38 -0800

    1/27 Commit 3

--- project-1/js/script.js
+++ project-1/js/script.js
@@ -15,16 +15,6 @@
 
 // });
 
-function allowDrop(ev) {
-  ev.preventDefault();
-}
-
-function drag(ev) {
-  ev.dataTransfer.setData("Text", ev.target.id);
-}
-
-function drop(ev) {
-  var data = ev.dataTransfer.getData("Text");
-  ev.target.appendChild(document.getElementById(data));
-  ev.preventDefault();
-}
+$( function() {
+	$( "#draggable" ).draggable({ axis: "y" });
+} );
