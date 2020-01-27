61bd7afdec53f9efeb853d1a1324de0cc4771e97
BuildTools <unconfigured@null.spigotmc.org>
2020-01-27 14:19:09 -0800

    1/27 Commit 1

--- project-1/js/script.js
+++ project-1/js/script.js
@@ -14,3 +14,17 @@
 // 	}
 
 // });
+
+function allowDrop(ev) {
+  ev.preventDefault();
+}
+
+function drag(ev) {
+  ev.dataTransfer.setData("Text", ev.target.id);
+}
+
+function drop(ev) {
+  var data = ev.dataTransfer.getData("Text");
+  ev.target.appendChild(document.getElementById(data));
+  ev.preventDefault();
+}
