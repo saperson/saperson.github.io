9f49ce6522ced2817779a3bfc906324efbf3e3fb
BuildTools <unconfigured@null.spigotmc.org>
2020-01-29 16:38:23 -0800

    1/29 Commit 3

--- project-1/js/script.js
+++ project-1/js/script.js
@@ -1,11 +1,3 @@
 $( function() {
 	$( "#draggable" ).draggable({ axis: "y" });
 } );
-
-$( function() {
-	$("#demoDrag").draggable({
-          stop: function() {
-          	 ("#page3-illo").addClass("illo-show");
-          }
-	});
-});
