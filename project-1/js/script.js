59d387fa41da4be920facf09a011a6284603fecc
BuildTools <unconfigured@null.spigotmc.org>
2020-02-05 14:40:47 -0800

    2/5 Commit 1

--- project-1/js/script.js
+++ project-1/js/script.js
@@ -1,3 +1,11 @@
 $( function() {
 	$( "#draggable" ).draggable({ axis: "y" });
 } );
+
+$( function() {
+	$( "#draggable" ).draggable({
+		stop: function() {
+			$("#page3illo").css("background-color", "red");
+		}
+	});
+});
