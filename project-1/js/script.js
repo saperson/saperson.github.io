95de802c88144d35bd87df56bb390eea22798774
BuildTools <unconfigured@null.spigotmc.org>
2020-02-07 15:47:14 -0800

    2/7 Commit 1

--- project-1/js/script.js
+++ project-1/js/script.js
@@ -7,6 +7,7 @@ $( function() {
 		stop: function() {
 			$("#sunset-img").css("background", "linear-gradient(rgba(255,107,237,1) 0%, rgba(255,159,39,1) 100%)");
 			$("#page3lines-container").css("background", "linear-gradient(rgba(255,107,237,1) 0%, rgba(255,159,39,1) 100%)");
+			$("#page3lines").css("opacity", "1");
 		}
 	});
 });
