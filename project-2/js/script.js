d4e32981556daea4b6a3008696f22a7413b03dbd
BuildTools <unconfigured@null.spigotmc.org>
2020-02-19 14:41:58 -0800

    2/19 Commit 1

--- project-2/js/script.js
+++ project-2/js/script.js
@@ -1,8 +1,23 @@
 $(function(){
 	$('.carousel').slick({
 	arrows: false,
-	dots: true,
+	dots: false,
 	slidesToShow: 1,
 	centerMode: true,
+	});
+});
+
+$(document).ready(function() {
+	// get current URL path and assign 'active' class
+	var pathname = window.location.pathname;
+	$('nav > a[href="'+pathname+'"]').parent().addClass('active');
+})
+
+$(function() {
+	$("a").click(function() {
+		// remove classes from all
+		$("a").removeClass("active");
+		// add class to the one we clicked
+		$(this).addClass("active");
 	});
 });
