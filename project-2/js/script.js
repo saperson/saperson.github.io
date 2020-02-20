9dfb6fda758519322393f2cae7b3c2054491a572
BuildTools <unconfigured@null.spigotmc.org>
2020-02-19 20:03:05 -0800

    2/19 Commit 2

--- project-2/js/script.js
+++ project-2/js/script.js
@@ -7,12 +7,6 @@ $(function(){
 	});
 });
 
-$(document).ready(function() {
-	// get current URL path and assign 'active' class
-	var pathname = window.location.pathname;
-	$('nav > a[href="'+pathname+'"]').parent().addClass('active');
-})
-
 $(function() {
 	$("a").click(function() {
 		// remove classes from all
@@ -21,3 +15,30 @@ $(function() {
 		$(this).addClass("active");
 	});
 });
+
+// $(document).ready(function() {
+// 	// get current URL path and assign 'active' class
+// 	var pathname = window.location.pathname;
+// 	$('nav > a[href="'+pathname+'"]').parent().addClass('active');
+// })
+
+// $(function() {
+//   $('nav a[href^="/' + location.pathname.split("/")[1] + '"]').addClass('active');
+// });
+
+$(function () {
+	setNavigation();
+});
+
+function setNavigation() {
+	var path = window.location.pathname;
+	path = path.replace(/\/$/, "");
+	path = decodeURIComponent(path);
+
+	$(".nav a").each(function () {
+		var href = $(this).attr('href');
+		if (path.substring(0, href.length) === href) {
+			$(this).closest('a').addClass('active');
+		}
+	});
+}
