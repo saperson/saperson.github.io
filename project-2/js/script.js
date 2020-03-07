66a97122c795dfeed6ec78769d751f790cfba58b
BuildTools <unconfigured@null.spigotmc.org>
2020-03-06 19:41:47 -0800

    3/6 Commit 1

--- project-2/js/script.js
+++ project-2/js/script.js
@@ -10,3 +10,15 @@ $(function(){
 $( function() {
 	$( "#draggable-insert" ).draggable({ axis: "y" });
 } );
+
+$("draggable-lid").click(function(){
+  $('#draggable-lid').css("transform", "rotate(-90deg);");
+});
+
+$('#grid-stripes').hover(
+	function(){ $('#gallery-label-stripes').toggleClass('gallery-label-active', '300') },
+)
+
+$('#grid-noods').hover(
+	function(){ $('#gallery-label-noods').toggleClass('gallery-label-active', '300') },
+)
