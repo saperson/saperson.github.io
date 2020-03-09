2aef972bfa6d55be4764c66d27f0104429a27588
BuildTools <unconfigured@null.spigotmc.org>
2020-03-09 15:28:42 -0700

    3/9 Commit 8

--- project-2/js/script.js
+++ project-2/js/script.js
@@ -1,6 +1,9 @@
 $(function(){
 	$('.carousel').slick({
-	arrows: false,
+	arrows: true,
+	appendArrows: $('.carousel'),
+	prevArrow: $('.left'),
+    nextArrow: $('.right'),
 	dots: false,
 	slidesToShow: 1,
 	centerMode: true,
@@ -8,45 +11,45 @@ $(function(){
 });
 
 $( function() {
-	$( "#draggable-insert" ).draggable({ axis: "y" });
+	$('#draggable-insert').draggable({ axis: 'y' });
 } );
 
-$("draggable-lid").click(function(){
-	$('#draggable-lid').css("transform", "rotate(-90deg);");
+$(".draggable-lid").click(function(){
+	$(this).animate({ transform: 'rotate(-90deg)'}, 500) ;
 });
 
 $('#grid-stripes').hover(
-	function(){ $('#gallery-label-stripes').toggleClass('gallery-label-active') },
+	function(){ $('#gallery-label-stripes').toggleClass('gallery-label-active', 100) },
 )
 
 $('#grid-noods').hover(
-	function(){ $('#gallery-label-noods').toggleClass('gallery-label-active') },
+	function(){ $('#gallery-label-noods').toggleClass('gallery-label-active', 100) },
 )
 
 $('#grid-gold').hover(
-	function(){ $('#gallery-label-gold').toggleClass('gallery-label-active') },
+	function(){ $('#gallery-label-gold').toggleClass('gallery-label-active', 100) },
 )
 
 $('#grid-gaming').hover(
-	function(){ $('#gallery-label-gaming').toggleClass('gallery-label-active') },
+	function(){ $('#gallery-label-gaming').toggleClass('gallery-label-active', 100) },
 )
 
 $('#grid-brown').hover(
-	function(){ $('#gallery-label-brown').toggleClass('gallery-label-active') },
+	function(){ $('#gallery-label-brown').toggleClass('gallery-label-active', 100) },
 )
 
 $('#grid-iridescent').hover(
-	function(){ $('#gallery-label-iridescent').toggleClass('gallery-label-active') },
+	function(){ $('#gallery-label-iridescent').toggleClass('gallery-label-active', 100) },
 )
 
 $('#grid-weave').hover(
-	function(){ $('#gallery-label-weave').toggleClass('gallery-label-active') },
+	function(){ $('#gallery-label-weave').toggleClass('gallery-label-active', 100) },
 )
 
 $('#grid-wood').hover(
-	function(){ $('#gallery-label-wood').toggleClass('gallery-label-active') },
+	function(){ $('#gallery-label-wood').toggleClass('gallery-label-active', 100) },
 )
 
 $('#grid-leprechaun').hover(
-	function(){ $('#gallery-label-leprechaun').toggleClass('gallery-label-active') },
+	function(){ $('#gallery-label-leprechaun').toggleClass('gallery-label-active', 100) },
 )
